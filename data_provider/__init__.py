3a40375c3871fe77e76dc7c2d9153a817c55edeb
cmagcian <minglingabc@aliyun.com>
2026-09-16 19:54:41 +0800

    Update module docstring to English

--- data_provider/__init__.py
+++ data_provider/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""数据源模块"""
+"""Data provider module."""
 
 from data_provider.base import BaseFetcher, DataFetchError
 from data_provider.akshare_fetcher import AkShareFetcher
